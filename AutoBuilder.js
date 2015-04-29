612e72497694d793352f782b287e599d272bd126
Richard Brammer <richard.brammer@geildanke.com>
2015-04-30 00:49:32 +0200

    FIX Only add scenes, if they are selected in the build settings FIX Automatically create directories, when creating a build

--- AutoBuilder.js
+++ AutoBuilder.js
@@ -36,117 +36,152 @@ import System.IO;
 
 public static class AutoBuilder {
 	
+	/**
+	 * Gets the Project name by looking up the directory name of the project
+	 * @return {String} Root directory name of the project
+	 */
 	function GetProjectName () {
 		var s : String[] = Application.dataPath.Split('/'[0]);
 		return s[s.Length - 2];
 	}
 	
+	/**
+	 * Gets the paths of the activated scenes from the BuildSettings
+	 * @return {String[]} C# BuiltinArray of Strings
+	 */
 	function GetScenePaths () {
-		var scenes = new String[EditorBuildSettings.scenes.Length];
+		var scenes = new Array();
 		
-		for(var i : int = 0; i < EditorBuildSettings.scenes.Length; i += 1)
-		{
-			scenes[i] = EditorBuildSettings.scenes[i].path;
+		for(var i : int = 0; i < EditorBuildSettings.scenes.Length; i += 1) {
+			
+			/**
+			 * Only add scene, if it is enabled in the build settings.
+			 */
+			if (EditorBuildSettings.scenes[i].enabled) {
+				scenes.Push(EditorBuildSettings.scenes[i].path);
+			}
 		}
 		
-		return scenes;
+		/**
+		 * Return a builtin array instead of a JavaScript Array
+		 */
+		var builtinArray : String[] = scenes.ToBuiltin(String) as String[];
+		return builtinArray;
 	}
-	
+
+	/**
+	 * Prepares the output directory and adds the Build to the BuildPipeline
+	 * @param {Srtring[]} scenes      Builtin array of Strings with the scenes to build
+	 * @param {String} outputFile        Output file or directory for the Player binary
+	 * @param {BuildTarget} buildTarget   BuildTarget Enumeration
+	 * @param {BuildOptions} buildOptions BuildOptions Enumeration
+	 */
+	function ExecuteBuild (scenes : String[], outputFile : String, buildTarget : BuildTarget, buildOptions: BuildOptions) {
+
+		/**
+		 * Get path from outputFile and create the path, if it does not exist.
+		 * Necessary for the first Build of a Project
+		 */
+		var path : String = Path.GetDirectoryName(outputFile);
+		Directory.CreateDirectory(path);
+
+		BuildPipeline.BuildPlayer(scenes, outputFile, buildTarget, buildOptions);
+	}
+
+
 	@MenuItem('File/AutoBuilder/Windows/32-bit')
 	function PerformWinBuild ()
 	{
-		BuildPipeline.BuildPlayer(GetScenePaths(), 'Builds/Win/' + GetProjectName() + '.exe',BuildTarget.StandaloneWindows,BuildOptions.None);
+		ExecuteBuild(GetScenePaths(), 'Builds/Win/' + GetProjectName() + '.exe',BuildTarget.StandaloneWindows,BuildOptions.None);
 	}
 	
 	@MenuItem('File/AutoBuilder/Windows/64-bit')
 	function PerformWin64Build ()
 	{
-		BuildPipeline.BuildPlayer(GetScenePaths(), 'Builds/Win64/' + GetProjectName() + '.exe',BuildTarget.StandaloneWindows64,BuildOptions.None);
+		ExecuteBuild(GetScenePaths(), 'Builds/Win64/' + GetProjectName() + '.exe',BuildTarget.StandaloneWindows64,BuildOptions.None);
 	}
 	
 	@MenuItem('File/AutoBuilder/Mac OSX/Universal')
 	function PerformOSXUniversalBuild ()
 	{
-		BuildPipeline.BuildPlayer(GetScenePaths(), 'Builds/OSX-Universal/' + GetProjectName() + '.app',BuildTarget.StandaloneOSXUniversal,BuildOptions.None);
+		ExecuteBuild(GetScenePaths(), 'Builds/OSX-Universal/' + GetProjectName() + '.app',BuildTarget.StandaloneOSXUniversal,BuildOptions.None);
 	}
 	
 	@MenuItem('File/AutoBuilder/Mac OSX/Intel')
 	function PerformOSXIntelBuild ()
 	{
-		BuildPipeline.BuildPlayer(GetScenePaths(), 'Builds/OSX-Intel/' + GetProjectName() + '.app',BuildTarget.StandaloneOSXIntel,BuildOptions.None);
+		ExecuteBuild(GetScenePaths(), 'Builds/OSX-Intel/' + GetProjectName() + '.app',BuildTarget.StandaloneOSXIntel,BuildOptions.None);
 	}
 	
 	@MenuItem('File/AutoBuilder/Mac OSX/Intel 64')
 	function PerformOSXIntel64Build ()
 	{
-		BuildPipeline.BuildPlayer(GetScenePaths(), 'Builds/OSX-Intel64/' + GetProjectName() + '.app',BuildTarget.StandaloneOSXIntel64,BuildOptions.None);
+		ExecuteBuild(GetScenePaths(), 'Builds/OSX-Intel64/' + GetProjectName() + '.app',BuildTarget.StandaloneOSXIntel64,BuildOptions.None);
 	}
 	
 	@MenuItem('File/AutoBuilder/iOS')
 	function PerformiOSBuild ()
 	{
-		BuildPipeline.BuildPlayer(GetScenePaths(), 'Builds/iOS',BuildTarget.iOS,BuildOptions.None);
+		ExecuteBuild(GetScenePaths(), 'Builds/iOS',BuildTarget.iOS,BuildOptions.None);
 	}
 	
 	@MenuItem('File/AutoBuilder/Android')
 	function PerformAndroidBuild ()
 	{
-		var path : String = 'Builds/Android';
-		Directory.CreateDirectory(path);
-		BuildPipeline.BuildPlayer(GetScenePaths(), path + '/' + GetProjectName() + '.apk', BuildTarget.Android,BuildOptions.None);
+		ExecuteBuild(GetScenePaths(), 'Builds/Android',BuildTarget.Android,BuildOptions.None);
 	}
 	
 	@MenuItem('File/AutoBuilder/Web/WebGL')
 	function PerformWebGLBuild ()
 	{
-		BuildPipeline.BuildPlayer(GetScenePaths(), 'Builds/WebGL',BuildTarget.WebGL,BuildOptions.None);
+		ExecuteBuild(GetScenePaths(), 'Builds/WebGL',BuildTarget.WebGL,BuildOptions.None);
 	}
 	
 	@MenuItem('File/AutoBuilder/Web/Standard')
 	function PerformWebBuild ()
 	{
-		BuildPipeline.BuildPlayer(GetScenePaths(), 'Builds/Web',BuildTarget.WebPlayer,BuildOptions.None);
+		ExecuteBuild(GetScenePaths(), 'Builds/Web',BuildTarget.WebPlayer,BuildOptions.None);
 	}
 	
 	@MenuItem('File/AutoBuilder/Web/Streamed')
 	function PerformWebStreamedBuild ()
 	{
-		BuildPipeline.BuildPlayer(GetScenePaths(), 'Builds/Web-Streamed',BuildTarget.WebPlayerStreamed,BuildOptions.None);
+		ExecuteBuild(GetScenePaths(), 'Builds/Web-Streamed',BuildTarget.WebPlayerStreamed,BuildOptions.None);
 	}
 	
 	@MenuItem('File/AutoBuilder/Playstation/PSP2')
 	function PerformPSP2Build ()
 	{
-		BuildPipeline.BuildPlayer(GetScenePaths(), 'Builds/Playstation/PSP2',BuildTarget.PSP2,BuildOptions.None);
+		ExecuteBuild(GetScenePaths(), 'Builds/Playstation/PSP2',BuildTarget.PSP2,BuildOptions.None);
 	}
 	
 	@MenuItem('File/AutoBuilder/Playstation/PS3')
 	function PerformPS3Build ()
 	{
-		BuildPipeline.BuildPlayer(GetScenePaths(), 'Builds/Playstation/PS3',BuildTarget.PS3,BuildOptions.None);
+		ExecuteBuild(GetScenePaths(), 'Builds/Playstation/PS3',BuildTarget.PS3,BuildOptions.None);
 	}
 	
 	@MenuItem('File/AutoBuilder/Playstation/PS4')
 	function PerformPS4Build ()
 	{
-		BuildPipeline.BuildPlayer(GetScenePaths(), 'Builds/Playstation/PS4',BuildTarget.PS4,BuildOptions.None);
+		ExecuteBuild(GetScenePaths(), 'Builds/Playstation/PS4',BuildTarget.PS4,BuildOptions.None);
 	}
 	
 	@MenuItem('File/AutoBuilder/Xbox/Xbox 360')
 	function PerformXbox360Build ()
 	{
-		BuildPipeline.BuildPlayer(GetScenePaths(), 'Builds/Xbox/Xbox-360',BuildTarget.XBOX360,BuildOptions.None);
+		ExecuteBuild(GetScenePaths(), 'Builds/Xbox/Xbox-360',BuildTarget.XBOX360,BuildOptions.None);
 	}
 	
 	@MenuItem('File/AutoBuilder/Xbox/Xbox One')
 	function PerformXboxOneBuild ()
 	{
-		BuildPipeline.BuildPlayer(GetScenePaths(), 'Builds/Xbox/Xbox-One',BuildTarget.XboxOne,BuildOptions.None);
+		ExecuteBuild(GetScenePaths(), 'Builds/Xbox/Xbox-One',BuildTarget.XboxOne,BuildOptions.None);
 	}
 	
 	@MenuItem('File/AutoBuilder/Windows Phone 8')
 	function PerformWP8Build ()
 	{
-		BuildPipeline.BuildPlayer(GetScenePaths(), 'Builds/Windows-Phone',BuildTarget.WP8Player,BuildOptions.None);
+		ExecuteBuild(GetScenePaths(), 'Builds/Windows-Phone',BuildTarget.WP8Player,BuildOptions.None);
 	}
 }
